99e597a8c84b4fb094f8b5bc82064cce4a727c82
wlaurance <w.laurance@gmail.com>
2014-02-21 20:17:16 -0500

    Compose key with cache id
    Fixes #1

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ module.exports = function(config){
   var client = redis.connect(config.url);
   var id = uuid.v1();
   cache.client = client;
+  function compose(k) {
+    return id + ':' + k;
+  }
   cache.set = function(key, value, cb){
-    client.set(key, value, function(err){
+    key = compose(key)
+    client.set(key, value, function(){
       client.expire(key, config.expiry, function(err){
-        if (typeof cb !== 'undefined')
+        if (typeof cb !== 'undefined') {
           cb(err);
+        }
       });
     });
   }
   cache.get = function(key, cb){
+    key = compose(key)
     client.get(key, cb);
   }
   cache.del = function(key, cb){
+    key = compose(key)
     client.del(key, cb);
   }
   cache.id = function() {
